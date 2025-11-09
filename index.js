a48bd1c092f7abd3ea098bae9799e86d8ac99b9b
MuNtAsIm FaHiM <muntasimfahim12@gmail.com>
2025-11-09 17:50:06 +0600

    git push

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //  Middleware
-app.use(cors());
+const corsOptions = {
+  origin: "https://food-euyv.vercel.app", // <-- তোমার frontend URL
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 //  MongoDB URI
